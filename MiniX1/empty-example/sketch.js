0646aaa739e04f44eed68746c842e748e28e8cda
nannastoerup <36166416+nannastoerup@users.noreply.github.com>
2018-02-09 10:33:47 +0100

    Add files via upload

---
+++ MiniX1/empty-example/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(700,500,WEBGL);
+}
+
+function draw() {
+  background('lightblue')
+  rotateX(frameCount * 0.02);
+  rotateY(frameCount * 0.02);
+  box(400,100,100);
+  stroke('grey')
+  fill('rgba(0,255,0,0.00)')
+  box(100,400,100);
+  box(100,100,400);
+
+}
